1f5a20af0908af5ec17e36f4a66ce5a57cfd6068
Abhishek <abhishekk@bsf.io>
2019-11-18 11:24:03 +0530

    file changed

--- bsf-readtime.php
+++ bsf-readtime.php
@@ -34,5 +34,3 @@ define('BSF_RT_PLUGIN_URL', untrailingslashit(plugins_url('', __FILE__)));
 
 require_once plugin_dir_path(__FILE__) . 'classes/class-bsfrt-loader.php';
 require_once plugin_dir_path(__FILE__) . 'classes/class-bsfrt-readtime.php';
-
-
